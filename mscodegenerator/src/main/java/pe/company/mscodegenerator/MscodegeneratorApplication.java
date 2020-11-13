603d2a3e9aa26e74ff553d8d4c3203f3b60325f1
andreirodriguez <arodriguez.impress@gmail.com>
2020-11-13 13:57:37 -0500

    Starter project

---
+++ mscodegenerator/src/main/java/pe/company/mscodegenerator/MscodegeneratorApplication.java
@@ -0,0 +1,13 @@
+package pe.company.mscodegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscodegeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscodegeneratorApplication.class, args);
+	}
+
+}
---
+++ mscodegenerator/src/test/java/pe/company/mscodegenerator/MscodegeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.company.mscodegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscodegeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
