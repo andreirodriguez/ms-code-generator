8034042403c5359ef3d4aa74199c055fea8d273b
andreirodriguez <arodriguez.impress@gmail.com>
2020-11-14 14:11:15 -0500

    Correcciones

--- mscodegenerator/src/main/java/pe/company/mscodegenerator/api/controller/TableController.java
+++ mscodegenerator/src/main/java/pe/company/mscodegenerator/api/controller/TableController.java
@@ -1,6 +1,5 @@
 package pe.company.mscodegenerator.api.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
--- mscodegenerator/src/main/java/pe/company/mscodegenerator/repository/implementation/TableRepositoryImp.java
+++ mscodegenerator/src/main/java/pe/company/mscodegenerator/repository/implementation/TableRepositoryImp.java
@@ -2,15 +2,10 @@ package pe.company.mscodegenerator.repository.implementation;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.simple.SimpleJdbcCall;
 import org.springframework.stereotype.Repository;
 
 import pe.company.mscodegenerator.repository.interfaces.TableRepositoryInt;
