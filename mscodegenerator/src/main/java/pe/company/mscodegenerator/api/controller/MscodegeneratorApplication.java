5042b16ca2667ab03c571e2842e8e1b1296cd29f
Andrei Sergio Rodriguez Barriga <arodriguez.impress@gmail.com>
2020-11-19 20:07:26 -0500

    AddCors

--- mscodegenerator/src/main/java/pe/company/mscodegenerator/api/controller/MscodegeneratorApplication.java
+++ mscodegenerator/src/main/java/pe/company/mscodegenerator/api/controller/MscodegeneratorApplication.java
@@ -3,6 +3,7 @@ package pe.company.mscodegenerator.api.controller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication(scanBasePackages = "pe.company.mscodegenerator")
 public class MscodegeneratorApplication {
 
